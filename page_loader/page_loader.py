69a4d63605cb5d4e719f95d2481a77e3fcf6bb07
Ilya Dutlov <id@dutlov.ru>
2022-02-15 18:50:09 +0300

    1 step: fix functions

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -2,22 +2,27 @@ import requests
 import re
 
 
-def download(url, path):
+def download(url, initial_path):
     r = requests.get(url)
     if r.ok:
         received_html = r.text
-        half_path_to_html = path
-        return place_html_to_path(url, received_html, half_path_to_html)
+        full_path = generate_full_path(url, initial_path)
+        write_file_to_path(full_path, received_html)
+        return full_path
     return None
 
 
-def place_html_to_path(url, html, half_path_to_html):
+def generate_full_path(url, initial_path):
     url_stripped = re.search(r'(?<=https://).*', url)
     if url_stripped:
         url_without_scheme = url_stripped.group()
         file_name = re.sub(r'[^a-zA-Z0-9]', '-', url_without_scheme) + '.html'
-        full_path_to_html = half_path_to_html + '/' + file_name
-        with open(full_path_to_html, 'w') as file:
-            file.write(html)
-        return full_path_to_html
+        full_path = initial_path + '/' + file_name
+        return full_path
     return None
+
+
+def write_file_to_path(full_path, html):
+    with open(full_path, 'w') as file:
+        file.write(html)
+    return
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,4 +1,9 @@
-
+import requests
+import requests_mock
+from page_loader.page_loader import download
 
 def test_page_loader():
     assert 2 == 2
+
+    file_path = download(url=url, path=output_path)
+    print(file_path)
