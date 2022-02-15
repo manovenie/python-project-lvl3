d3b3202189dcbf109751c05ccb09a97d2dcbb877
Ilya Dutlov <id@dutlov.ru>
2022-02-15 19:32:58 +0300

    fix test

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -6,8 +6,9 @@ from page_loader.page_loader import download
 TEST_URL = 'https://ru.hexlet.io/courses'
 
 def test_page_loader(requests_mock):
-    requests_mock.get(TEST_URL, text='data')
-    assert 'data' == requests.get(TEST_URL).text
+    requests_mock.get(TEST_URL)
+    res = requests.get(TEST_URL)
+    assert res.status_code == 200
 
 '''
 def test_page_loader(requests_mock):
