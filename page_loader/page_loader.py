a4fbd89f6d9f637269bb5573a51f62a87e3d7a7c
Ilya Dutlov <id@dutlov.ru>
2022-02-18 00:03:11 +0300

    add sketch for 2 step

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -6,8 +6,11 @@ def download(url, initial_path):
     r = requests.get(url)
     if r.ok:
         received_html = r.content
-        full_path = generate_full_path(url, initial_path)
-        write_file_to_path(full_path, received_html)
+        folder_img_path, full_html_path = generate_full_path(url, initial_path)
+        write_file_to_path(full_html_path, received_html)
+        # create folder for img
+        # download img via tags and place them in folder
+        # change links to imgs in generated file
         return full_path
     return None
 
@@ -16,9 +19,10 @@ def generate_full_path(url, initial_path):
     url_stripped = re.search(r'(?<=https://).*', url)
     if url_stripped:
         url_without_scheme = url_stripped.group()
-        file_name = re.sub(r'[^a-zA-Z0-9]', '-', url_without_scheme) + '.html'
-        full_path = initial_path + '/' + file_name
-        return full_path
+        file_name = re.sub(r'[^a-zA-Z0-9]', '-', url_without_scheme)
+        folder_img_path = initial_path + '/' + file_name + '_files'
+        full_html_path = initial_path + '/' + file_name + '.html'
+        return folder_img_path, full_html_path
     return None
 
 
