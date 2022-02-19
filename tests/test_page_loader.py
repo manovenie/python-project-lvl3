a12489f4685a8dbe30ea7c649e146135517ad656
Ilya Dutlov <id@dutlov.ru>
2022-02-20 00:52:20 +0300

    add minor tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,17 +1,40 @@
-import pytest
-import requests
-import requests_mock
+#!/usr/bin/env python3
+
+from page_loader import download
+from page_loader.page_loader import format_local_name, upload_files
 import tempfile
+import pytest
 import os
-from page_loader.page_loader import download
-from pathlib import Path, PurePath
 
-TEST_URL = 'https://ru.hexlet.io/'
-HTML_FIXTURE = 'tests/fixtures/hexlet.html'
 
-def test_page_loader(requests_mock, tmpdir):
-    requests_mock.get(TEST_URL)
-    real_html = download(TEST_URL, tmpdir)
-    real_page = Path(PurePath(real_html)).read_bytes()
-    expected_page = Path(PurePath(HTML_FIXTURE)).read_bytes()
-    assert real_page == expected_page
+@pytest.mark.parametrize('URL, get_name, file_status, dir_status,', [
+    ('https://github.com/dutlov/python-project-lvl3',
+     'github-com-dutlov-python-project-lvl3.html', None, None),
+    ('https://github.com/K0Hb/python-project-lvl3',
+     'github-com-dutlov-python-project-lvl3_files', None, True),
+    ('https://github.com/K0Hb/python-project-lvl3.css',
+     'github-com-dutlov-python-project-lvl3.css', True, None)
+])
+def test_get_name(URL, get_name, dir_status, file_status):
+    assert format_local_name(URL, file=file_status,
+                             dir=dir_status) == get_name
+
+
+def test_load_files():
+    with tempfile.TemporaryDirectory() as temp:
+        link_for_test = 'https://github.com/dutlov/python-project-lvl3'
+        path = os.path.join(temp, format_local_name(link_for_test, 'file'))
+        upload_files([(link_for_test, path)])
+        assert os.path.isfile(path)
+
+
+@pytest.mark.parametrize('URL, path, exception', [
+    ('dutlov.github.io/github.io/', '/error_path/', 'Wrong address!'),
+    ('ht://dutlov.github.io/github.io/', '/error_path/', 'Wrong address!'),
+    ('http://httpdqwasd.org/status/404', '/error_path/', 'Connection failed'),
+    ('https://github.com/dutlov/python-project-lvl3',
+     'error_path', 'Your folder is incorrect')
+])
+def test_errors(URL, path, exception):
+    with pytest.raises(Exception):
+        download(URL, path)
