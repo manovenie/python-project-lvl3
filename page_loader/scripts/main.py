371a610da1d0541ba8cfed65c56da8fbc914cf13
Ilya Dutlov <id@dutlov.ru>
2022-02-20 14:33:03 +0300

    fix errors

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -1,7 +1,9 @@
 # !/usr/bin/env python3
+
 from page_loader.cli import parse_cli_args
 from page_loader.page_loader import download
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -13,12 +15,12 @@ def main():
     output_path = arguments.output
     try:
         file_path = download(url, output_path)
-        print(f'Page saved in: {file_path})
+        print(f'Page saved in: {file_path}')
     except Exception:
         logging.error('Error')
         sys.exit(1)
     else:
-        sys.exit(0))
+        sys.exit(0)
 
 
 if __name__ == '__main__':
